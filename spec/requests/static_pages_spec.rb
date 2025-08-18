539dc4beac7b8a69b0d7248863a706fd99ba03c4
Annenpolka <lancelbb@gmail.com>
2025-08-18 22:14:45 +0900

    Contactページを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
+  before(:all) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET /home" do
     it "returns http success" do
       get "/static_pages/home"
@@ -9,7 +13,7 @@ RSpec.describe "StaticPages", type: :request do
     it "titleが正しい" do
       get  "/static_pages/home"
       dom = Capybara.string(response.body)
-      expect(dom).to have_title("Home | Ruby on Rails Tutorial Sample App")
+      expect(dom).to have_title("Home | #{@base_title}")
     end
   end
 
@@ -21,7 +25,7 @@ RSpec.describe "StaticPages", type: :request do
     it "titleが正しい" do
       get  "/static_pages/help"
       dom = Capybara.string(response.body)
-      expect(dom).to have_title("Help | Ruby on Rails Tutorial Sample App")
+      expect(dom).to have_title("Help | #{@base_title}")
     end
   end
 
@@ -33,8 +37,19 @@ RSpec.describe "StaticPages", type: :request do
     it "titleが正しい" do
       get  "/static_pages/about"
       dom = Capybara.string(response.body)
-      expect(dom).to have_title("About | Ruby on Rails Tutorial Sample App")
+      expect(dom).to have_title("About | #{@base_title}")
     end
   end
 
+  describe "GET /contact" do
+    it "returns http success" do
+      get "/static_pages/contact"
+      expect(response).to have_http_status(:success)
+    end
+    it "titleが正しい" do
+      get  "/static_pages/contact"
+      dom = Capybara.string(response.body)
+      expect(dom).to have_title("Contact | #{@base_title}")
+    end
+  end
 end
