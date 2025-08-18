190074d1a03f2163c11c9c5d75631c62f5b98484
Annenpolka <lancelbb@gmail.com>
2025-08-18 22:02:56 +0900

    StaticPagesのタイトルをテンプレート化し、RSpecテストを更新

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe "StaticPages", type: :request do
       get "/static_pages/home"
       expect(response).to have_http_status(:success)
     end
+    it "titleが正しい" do
+      get  "/static_pages/home"
+      dom = Capybara.string(response.body)
+      expect(dom).to have_title("Home | Ruby on Rails Tutorial Sample App")
+    end
   end
 
   describe "GET /help" do
@@ -13,12 +18,22 @@ RSpec.describe "StaticPages", type: :request do
       get "/static_pages/help"
       expect(response).to have_http_status(:success)
     end
+    it "titleが正しい" do
+      get  "/static_pages/help"
+      dom = Capybara.string(response.body)
+      expect(dom).to have_title("Help | Ruby on Rails Tutorial Sample App")
+    end
   end
 
   describe "GET /about" do
     it "returns http success" do
       get "/static_pages/about"
       expect(response).to have_http_status(:success)
+    end
+    it "titleが正しい" do
+      get  "/static_pages/about"
+      dom = Capybara.string(response.body)
+      expect(dom).to have_title("About | Ruby on Rails Tutorial Sample App")
     end
   end
 
