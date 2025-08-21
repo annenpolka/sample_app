c64d5550d0d48a224d7db1e0d3197cf2f119847f
Annenpolka <lancelbb@gmail.com>
2025-08-21 11:21:12 +0900

    ユーザー登録失敗の実装

---
+++ spec/requests/users_signup_spec.rb
@@ -0,0 +1,25 @@
+# spec/requests/users_signup_spec.rb
+require "rails_helper"
+
+RSpec.describe "Users signup", type: :request do
+  it "invalid signup information" do
+    get signup_path
+
+    expect {
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password_confirmation: "bar"
+        }
+      }
+    }.not_to change(User, :count)
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(response).to render_template("users/new")
+    dom = Capybara.string(response.body)
+    expect(dom).to have_css('div#error_explanation')
+    expect(dom).to have_css('div.field_with_errors')
+  end
+end
