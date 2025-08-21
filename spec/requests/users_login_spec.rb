16a7b40feca98e864d4da72e3c066c7865863fc9
Annenpolka <lancelbb@gmail.com>
2025-08-21 16:21:52 +0900

    テストのリファクタリング

--- spec/requests/users_login_spec.rb
+++ spec/requests/users_login_spec.rb
@@ -1,69 +1,101 @@
 require 'rails_helper'
 
-RSpec.describe "Users Login", type: :request do
-  before(:each) do
-    @user = create(:john)
+RSpec.describe "ユーザーログイン", type: :request do
+  let(:user) { create(:john) }
+
+  # Small helpers to keep examples focused
+  def dom_for(response)
+    Capybara.string(response.body)
   end
 
-  describe "/login" do
-    it "login with valid information" do
-      post login_path, params: { session: { email:    @user.email,
-                                            password: @user.password } }
-      expect(response).to be_redirect
-      expect(response).to redirect_to(user_path(User.last))
-      follow_redirect!
-      expect(response).to render_template("users/show")
-      dom = Capybara.string(response.body)
+  def expect_nav_for_logged_in(dom)
+    aggregate_failures do
       expect(dom).to have_no_link(href: login_path)
       expect(dom).to have_link(href: logout_path)
-      expect(dom).to have_link(href: user_path(@user))
+      expect(dom).to have_link(href: user_path(user))
     end
+  end
 
-    it "login with invalid information" do
-      get login_path
-      expect(response).to render_template("sessions/new")
-      post login_path, params: { session: { email: "", password: "" } }
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response).to render_template("sessions/new")
-      expect(flash).not_to be_empty
-      get root_path
-      expect(flash).to be_empty
+  def expect_nav_for_guest(dom)
+    aggregate_failures do
+      expect(dom).to have_link(href: login_path)
+      expect(dom).to have_no_link(href: logout_path)
+      expect(dom).to have_no_link(href: user_path(user))
     end
+  end
 
-    it "login with valid email/invalid password" do
-      get login_path
-      expect(response).to render_template("sessions/new")
-      post login_path, params: { session: { email: @user.email, password: "invalid" } }
-      expect(is_logged_in?).not_to be_truthy
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response).to render_template("sessions/new")
-      expect(flash).not_to be_empty
-      get root_path
-      expect(flash).to be_empty
+  def post_login_as(user)
+    post login_path, params: { session: { email: user.email, password: user.password } }
+  end
+
+  describe "ログイン（/login）" do
+    describe "ログイン（POST /login）" do
+      context "認証情報が正しい場合" do
+        it "ユーザーのプロフィールへリダイレクトし、ログイン状態のナビゲーションが表示される" do
+          post_login_as(user)
+
+          expect(is_logged_in?).to be_truthy
+          expect(response).to redirect_to(user_path(user))
+
+          follow_redirect!
+          expect(response).to render_template("users/show")
+          expect_nav_for_logged_in(dom_for(response))
+        end
+      end
+
+      context "メールアドレス・パスワードが無効な場合" do
+        it "422でフォームを再表示し、フラッシュを表示後に消去する" do
+          get login_path
+          expect(response).to render_template("sessions/new")
+
+          post login_path, params: { session: { email: "", password: "" } }
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(response).to render_template("sessions/new")
+          expect(flash).not_to be_empty
+
+          get root_path
+          expect(flash).to be_empty
+        end
+      end
+
+      context "メールアドレスは有効だがパスワードが無効な場合" do
+        it "ログインせず、422でフォームを再表示し、フラッシュを表示後に消去する" do
+          get login_path
+          expect(response).to render_template("sessions/new")
+
+          post login_path, params: { session: { email: user.email, password: "invalid" } }
+          expect(is_logged_in?).not_to be_truthy
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(response).to render_template("sessions/new")
+          expect(flash).not_to be_empty
+
+          get root_path
+          expect(flash).to be_empty
+        end
+      end
     end
+  end
 
-    it "login with valid information followed by logout" do
-      post login_path, params: { session: { email:    @user.email,
-                                            password: @user.password } }
-      expect(is_logged_in?).to be_truthy
-      expect(response).to redirect_to(user_path(User.last))
-      follow_redirect!
-      expect(response).to render_template("users/show")
-      login_dom = Capybara.string(response.body)
-      expect(login_dom).to have_no_link(href: login_path)
-      expect(login_dom).to have_link(href: logout_path)
-      expect(login_dom).to have_link(href: user_path(@user))
-      delete logout_path
-      expect(is_logged_in?).not_to be_truthy
-      expect(response).to have_http_status(:see_other)
-      expect(response).to redirect_to(root_url)
-      follow_redirect!
-      logout_dom = Capybara.string(response.body)
-      expect(logout_dom).to have_link(href: login_path)
-      expect(logout_dom).to have_no_link(href: logout_path)
-      expect(logout_dom).to have_no_link(href: user_path(@user))
+  describe "ログアウト（DELETE /logout）" do
+    context "ログイン済みの場合" do
+      before do
+        post_login_as(user)
+        follow_redirect!
+      end
 
+      it "ログアウトして303でトップへリダイレクトする" do
+        delete logout_path
+        expect(is_logged_in?).not_to be_truthy
+        expect(response).to have_http_status(:see_other)
+        expect(response).to redirect_to(root_url)
+      end
+
+      it "ログアウト後はゲスト用ナビゲーションが表示される" do
+        delete logout_path
+        follow_redirect!
+
+        expect_nav_for_guest(dom_for(response))
+      end
     end
-
   end
 end
