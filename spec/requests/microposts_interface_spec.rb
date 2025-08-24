9824d3418f3c35ff87a3ecd76b703ad5cfa678c7
Annenpolka <lancelbb@gmail.com>
2025-08-24 17:35:21 +0900

    マイクロポストの操作の実装

---
+++ spec/requests/microposts_interface_spec.rb
@@ -0,0 +1,87 @@
+# spec/requests/microposts_interface_spec.rb
+require "rails_helper"
+
+RSpec.describe "Microposts interface", type: :request do
+  let!(:user)       { create(:user, password: "password", password_confirmation: "password") }
+  let!(:other_user) { create(:user) }
+  let!(:microposts) { create_list(:micropost, 35, user: user) }
+
+  before do
+    log_in_as(user)
+  end
+
+  def dom
+    Capybara.string(response.body)
+  end
+
+  context "マイクロポスト画面の表示" do
+    it "should paginate microposts" do
+      get root_path
+      expect(dom).to have_css('div.pagination')
+    end
+  end
+
+  context "マイクロポストの作成" do
+    it "should show errors but not create micropost on invalid submission" do
+      expect {
+        post microposts_path, params: { micropost: { content: "" } }
+      }.not_to change(Micropost, :count)
+      expect(dom).to have_css('div#error_explanation')
+      expect(dom).to have_link('2', href: root_path(page: 2))
+    end
+
+    it "creates on valid submission" do
+      content = "This micropost really ties the room together"
+      expect {
+        post microposts_path, params: { micropost: { content: content } }
+      }.to change(Micropost, :count).by(1)
+      expect(response).to redirect_to(root_url)
+      follow_redirect!
+      expect(response.body).to include(content)
+    end
+  end
+
+  context "マイクロポストの削除" do
+    let!(:micropost) { create(:micropost, user: user) }
+    let!(:other_user_micropost) { create(:micropost, user: other_user) }
+
+    it "shows delete links on own profile page" do
+      get user_path(user)
+      expect(dom).to have_css('a', text: 'delete')
+    end
+
+    it "should be able to delete own micropost" do
+      expect {
+        delete micropost_path(micropost)
+    }.to change(Micropost, :count).by(-1)
+    end
+
+    it "should not have delete links on other user's profile page" do
+      get user_path(other_user)
+      expect(dom).not_to have_css('a', text: 'delete')
+    end
+  end
+
+  context "マイクロポストのサイドバーの表示" do
+    let!(:no_microposts_user) { create(:user) }
+    let!(:one_micropost_user) { create(:user) }
+    let!(:one_micropost) { create(:micropost, user: one_micropost_user) }
+
+    it "should display the right micropost count" do
+      get root_path
+      expect(response.body).to match("#{microposts.count} microposts")
+    end
+
+    it "should user proper pluralization for zero microposts" do
+      log_in_as(no_microposts_user)
+      get root_path
+      expect(response.body).to match("0 microposts")
+    end
+    it "should user proper pluralization for one micropost" do
+      log_in_as(one_micropost_user)
+      get root_path
+      expect(response.body).to match("1 micropost")
+    end
+  end
+
+end
--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -1,7 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Microposts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let!(:micropost) { create(:micropost) }
+  describe "/micropost" do
+    context "ログアウト時のマイクロポストの操作" do
+      it "should redirect create when not logged in" do
+        expect { post microposts_path }.not_to change(Micropost, :count)
+        expect(response).to redirect_to(login_url)
+      end
+
+      it "should redirect destroy when not logged in" do
+        expect { delete micropost_path(micropost) }.not_to change(Micropost, :count)
+        expect(response).to have_http_status(:see_other)
+        expect(response).to redirect_to(login_url)
+      end
+    end
+  end
+
+  context "異なるユーザーのマイクロポストの操作" do
+    let(:user) { create(:user) }
+    let(:other_user) { create(:user) }
+    let!(:micropost) { create(:micropost, user: other_user) }
+    it "should redirect destroy for wrong micropost" do
+      log_in_as(user)
+      expect { delete micropost_path(micropost) }.not_to change(Micropost, :count)
+      expect(response).to have_http_status(:see_other)
+      expect(response).to redirect_to(root_url)
+    end
   end
 end
